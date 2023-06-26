2eb4933fe93bd68edf3f0bbd3a1121078afee0b3
Nataliya Krasyuk <116311212+NataliyaKr@users.noreply.github.com>
2023-06-26 21:17:16 +0200

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector("[name='email']");
+const message = document.querySelector("[name='message']");
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener("submit", onFormSubmit);
+
+function onFormInput() {
+    const params = {
+        email: email.value,
+        message: message.value
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(params));
+};
+
+const data = localStorage.getItem(STORAGE_KEY);
+const parseData = JSON.parse(data);
+
+if (parseData) {
+    email.value = parseData.email;
+    message.value = parseData.message;
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
